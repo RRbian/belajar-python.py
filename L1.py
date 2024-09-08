1007664d41e0614a36a72c2a83d347269d6b1be5
RRbian <riorobiyan@gmail.com>
2024-09-08 12:55:42 +0700

    Update L1.py

--- L1.py
+++ L1.py
@@ -7,3 +7,17 @@ if Umur < 30:
     print("Masih Muda")
 else:
     print("Sudah Tua")
+
+#latihan while Loop
+i = 0
+while i <= 10:
+    print(i)
+    i += 1
+
+#latihan Fungsi
+def luasPersegi (angka):
+    return angka * angka
+    
+sisi = luasPersegi(6)
+print(sisi)
+
